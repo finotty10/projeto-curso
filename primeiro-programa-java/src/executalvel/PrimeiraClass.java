badf42198942b3131bd94ad5eeb4bd4336de87dc
Usuário <Usurio@10.0.0.100>
2022-01-16 03:02:07 -0300

    crianção do projeto

---
+++ primeiro-programa-java/src/executalvel/PrimeiraClass.java
@@ -0,0 +1,14 @@
+package executalvel;
+
+import java.util.Iterator;
+
+public class PrimeiraClass {
+ 
+	public static void main(String[] args) {
+		System.out.println("tabuada de 9");
+		for(int i=1; i<=10; i++) {
+			System.out.println("9 x "+i+" = "+(9*i));
+			
+		}
+	}
+}
