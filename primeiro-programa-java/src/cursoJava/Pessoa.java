cbf619f07598c37089b5d01f35209726d0e0405e
Usuário <Usurio@10.0.0.100>
2022-01-17 15:44:10 -0300

    Revert "incremento date"
    This reverts commit bcdb373817983a7a84a256ff108239b6424ecc4f.

--- primeiro-programa-java/src/cursoJava/Pessoa.java
+++ primeiro-programa-java/src/cursoJava/Pessoa.java
@@ -1,13 +1,10 @@
 package cursoJava;
 
-import java.util.Date;
-
 public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
 	private int idade=0;
-	private Date dataNascimento;
 	
 	
 	//teste
