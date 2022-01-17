8029f859d24035dd8fd9f658d289be41b46adbfe
Usuário <Usurio@10.0.0.100>
2022-01-17 15:28:57 -0300

    getters e setters de idade

--- primeiro-programa-java/src/cursoJava/Pessoa.java
+++ primeiro-programa-java/src/cursoJava/Pessoa.java
@@ -10,6 +10,12 @@ public class Pessoa {
 	//teste
 	
 	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return nome;
 	}
