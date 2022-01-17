014212387daad565bcb85d4c55747611548b5449
Usuário <Usurio@10.0.0.100>
2022-01-17 15:26:24 -0300

    testeeee

--- primeiro-programa-java/src/cursoJava/Pessoa.java
+++ primeiro-programa-java/src/cursoJava/Pessoa.java
@@ -1,5 +1,28 @@
 package cursoJava;
 
 public class Pessoa {
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+	//teste
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+	
 
 }
