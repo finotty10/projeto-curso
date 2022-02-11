f13ee463dd509af569a4cf9825c55f20c7f03280
Usuário <Usurio@10.0.0.102>
2022-02-11 01:03:07 -0300

    Thread em objeto

--- primeiro-programa-java/src/cursoJava/threads/aulaThread.java
+++ primeiro-programa-java/src/cursoJava/threads/aulaThread.java
@@ -5,9 +5,50 @@ import javax.swing.JOptionPane;
 public class aulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
-		//Thread processando em paralelo
-		new Thread() {
-			public void run() {//executa o que nós queremos
+		//Thread processando em paralelo envio de email
+		Thread threadEmail = new Thread(thread1); 
+		threadEmail.start();
+		
+		//.........................divisao....................................
+		
+	
+		Thread threadNota = new Thread(thread2);
+		threadNota.start();
+		
+		
+		//codigo do sistema do usuario continua o fluxo de trabalho
+		System.out.println("chegou ao fim :D");
+		//fluxo do sistema, cadastro de venda, uma emissao de nota fiscal
+		//JOptionPane.showMessageDialog(null, "Sistema continua executando para usuario");
+	}
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			//executa o que nós queremos
+			//codigo da rotina
+			//codigo da rotina que eu quero executar em paralelo
+			for(int pos=0; pos < 10; pos ++) {
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {						
+					e.printStackTrace();
+				}
+				/*Quero executar esse envio com tempo de parada ou com tempo determinado*/
+				System.out.println("Executando alguma rotina, envio de e-mail");
+				//fim do codigo em paralelo
+			}
+		}
+			
+		};
+		
+		private static Runnable thread2 = new Runnable() {
+			
+			@Override
+			public void run() {
+				
 				//codigo da rotina
 				//codigo da rotina que eu quero executar em paralelo
 				for(int pos=0; pos < 10; pos ++) {
@@ -18,17 +59,11 @@ public class aulaThread {
 						e.printStackTrace();
 					}
 					/*Quero executar esse envio com tempo de parada ou com tempo determinado*/
-					System.out.println("Executando alguma rotina, envio de e-mail");
+					System.out.println("Executando alguma rotina, envio de nota fiscal");
 					//fim do codigo em paralelo
 				}
+				
 			}
-			
-		}.start();//Start liga a Thread que fica processando paralelamente
-		
-		
-		//codigo do sistema do usuario continua o fluxo de trabalho
-		System.out.println("chegou ao fim :D");
-		//fluxo do sistema, cadastro de venda, uma emissao de nota fiscal
-		//JOptionPane.showMessageDialog(null, "Sistema continua executando para usuario");
-	}
+		};
+	
 }
